d270c0978ca2a91bf6f3a575e32f8aab7be913b1
rootools <baboonweb@gmail.com>
2013-05-02 17:14:21 +0400

    complite profile page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('achi', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
+    when('/profile', {templateUrl: 'profile.html'/*, controller: FriendsListCtrl*/}).
     when('/dashboard', {templateUrl: 'dashboard.html'/*, controller: FriendsListCtrl*/}).
     when('/friends', {templateUrl: 'friendslist.html'/*, controller: FriendsListCtrl*/}).
     otherwise({redirectTo: '/'});
