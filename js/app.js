6c1e89a7baea980cfc66445a24213963d2ff8a35
rootools <baboonweb@gmail.com>
2013-05-04 01:12:23 +0400

    add top page

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ angular.module('achi', []).
     when('/profile', {templateUrl: 'profile.html'/*, controller: FriendsListCtrl*/}).
     when('/dashboard', {templateUrl: 'dashboard.html'/*, controller: FriendsListCtrl*/}).
     when('/feed', {templateUrl: 'feed.html'/*, controller: FriendsListCtrl*/}).
+    when('/top', {templateUrl: 'top.html'/*, controller: FriendsListCtrl*/}).
     when('/friends', {templateUrl: 'friendslist.html'/*, controller: FriendsListCtrl*/}).
     otherwise({redirectTo: '/'});
 }]);
