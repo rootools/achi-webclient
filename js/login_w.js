375f153b7fa64c6983a044b542015a85020e251c
rootools <baboonweb@gmail.com>
2013-05-23 01:43:59 +0400

    complite form scripting

--- js/login_w.js
+++ js/login_w.js
@@ -5,9 +5,9 @@ login_chooser.onclick = function(e) {
   login_chooser.className = 'chooser chooser_selected';
   reg_chooser.className = 'chooser';
   var form = document.getElementById('login_or_reg_form');
-  var html = '<input type="text" placeholder="E-mail">' +
-             '<input type="password" placeholder="Пароль">' +
-             '<button id="login_button_login">Войти</button>' +
+  var html = '<input type="text" placeholder="E-mail" name="email" required>' +
+             '<input type="password" placeholder="Пароль" name="pass" required>' +
+             '<input type="submit" id="login_button_login" value="Войти">' +
              '<span id="login_recover_link"><a href="/">Забыли пароль?</a></span>';
 
   form.innerHTML = html;
@@ -17,10 +17,18 @@ reg_chooser.onclick = function(e) {
   reg_chooser.className = 'chooser chooser_selected';
   login_chooser.className = 'chooser';
   var form = document.getElementById('login_or_reg_form');
-  var html = '<input type="text" placeholder="E-mail">' +
-             '<input type="password" placeholder="Пароль">' +
-             '<input type="password" placeholder="Еще раз пароль">' +
-             '<button id="login_button_reg">Зарегистрироваться</button>';
+  var html = '<input type="text" placeholder="E-mail" name="email" required>' +
+             '<input type="password" placeholder="Пароль" name="pass" required id="regPass">' +
+             '<input type="password" placeholder="Еще раз пароль" oninput="check_pass(this)" required>' +
+             '<input type="submit" id="login_button_reg" value="Зарегистрироваться">';
 
   form.innerHTML = html;
 };
+
+function check_pass(input) {
+  if (input.value != document.getElementById('regPass').value) {
+    input.setCustomValidity('Пароли не совпадают');
+  } else {
+    input.setCustomValidity('');
+  }
+}
